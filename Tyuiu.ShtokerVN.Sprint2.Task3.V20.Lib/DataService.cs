3b61729f533eef2baa4d88f3e50595785ba7f291
Vladimir Shtoker <Tyuiu.ShtokerVN@outlook.com>
2023-10-14 17:36:36 +0500

    Task3

---
+++ Tyuiu.ShtokerVN.Sprint2.Task3.V20.Lib/DataService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tyuiu.cources.programming.interfaces.Sprint2;
+
+namespace Tyuiu.ShtokerVN.Sprint2.Task3.V20.Lib
+{
+    public class DataService : ISprint2Task3V20
+    {
+        public double Calculate(double x)
+        {
+            double y = 0;
+
+            if (x > 1)
+            {
+                y = x * Math.Pow(((x + 1) / (x - 1)), x);
+            }
+            else
+            {
+                if (x == 0)
+                {
+                    y = ((Math.Sqrt(x) - Math.Sqrt(Math.Cos(x)) + 10) / (Math.Sqrt(x) - Math.Sqrt(Math.Sin(x)) + 12));
+                }
+                else
+                {
+                    if ((x > -24) && (x < 2))
+                    {
+                        y = Math.Pow(1 + 1 / Math.Sqrt(x), x);
+                    }
+                    else
+                    {
+                        if (x < -24)
+                        {
+                            y = x + 10 * x - (1 / x);
+                        }
+                    }
+                }
+            }
+            return Math.Round(y, 3);
+        }
+    }
+}
---
+++ Tyuiu.ShtokerVN.Sprint2.Task3.V20/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.ShtokerVN.Sprint2.Task3.V20.Lib;
+
+namespace Tyuiu.ShtokerVN.Sprint2.Task3.V20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.WriteLine("*********************************************************************************************************************");
+            Console.WriteLine("* Спринт #2                                                                                                         *");
+            Console.WriteLine("* Тема: Базовые навыки работы C#                                                                                    *");
+            Console.WriteLine("* Задание #3                                                                                                        *");
+            Console.WriteLine("* Вариант #20                                                                                                       *");
+            Console.WriteLine("* Выполнил: Штокер Владимир Николаевич | ИИПБ-23-1                                                                  *");
+            Console.WriteLine("*********************************************************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                                                          *");
+            Console.WriteLine("* Написать программу, которая  вычисляет требуемое значение функции Y с использованием вложенных оператор if-else,  *");
+            Console.WriteLine("* где пользователь вводит значение переменной X с клавиатуры.                                                       *");
+            Console.WriteLine("*                                                                                                                   *");
+            Console.WriteLine("*********************************************************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                                                                  *");
+            Console.WriteLine("*********************************************************************************************************************");
+            Console.WriteLine("    x(x + 1/ x - 1) ^ x , если x > 1");
+            Console.WriteLine("    x^2 - cos(x)^2 + 10 / x^2 - sin(x)^2 + 12 , если x = 0");
+            Console.WriteLine("y =");
+            Console.WriteLine("    (1 + 1/ x^2)^x , если -24 < x < 2");
+            Console.WriteLine("    x + 10*x - (1/x) , если x < -24");
+
+            Console.WriteLine("Введите значение переменной X: ");
+            double x = Convert.ToDouble(Console.ReadLine());
+            double res = ds.Calculate(x);
+
+
+            Console.WriteLine("*********************************************************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                                                        *");
+            Console.WriteLine("*********************************************************************************************************************");
+
+            Console.WriteLine("Значение функции =" + res);
+            Console.ReadKey();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
