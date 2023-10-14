27473071250bb22729597db6b58ce60dd341e290
Vladimir Shtoker <Tyuiu.ShtokerVN@outlook.com>
2023-10-14 18:23:44 +0500

    Task4

---
+++ Tyuiu.ShtokerVN.Sprint2.Task4.V29.Test/DataServiceTest.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.ShtokerVN.Sprint2.Task4.V29.Lib;
+
+namespace Tyuiu.ShtokerVN.Sprint2.Task4.V29.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCondition1()
+        {
+            DataService ds = new DataService();
+            double x = 10;
+            double y = 5;
+            double res = ds.Calculate(x, y);
+            double wait = 99.6;
+            Assert.AreEqual(wait, res);
+        }
+    
+        [TestMethod]
+        public void ValidCondition2()
+        {
+            DataService ds = new DataService();
+            double x = 10;
+            double y = 16;
+            double res = ds.Calculate(x, y);
+            double wait = 99.875;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.ShtokerVN.Sprint2.Task4.V29/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.ShtokerVN.Sprint2.Task4.V29.Lib;
+
+namespace Tyuiu.ShtokerVN.Sprint2.Task4.V29
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.WriteLine("*****************************************************************************************************");
+            Console.WriteLine("* Спринт #2                                                                                         *");
+            Console.WriteLine("* Тема: Базовые навыки работы C#                                                                    *");
+            Console.WriteLine("* Задание #4                                                                                        *");
+            Console.WriteLine("* Вариант #29                                                                                       *");
+            Console.WriteLine("* Выполнил: Штокер Владимир Николаевич | ИИПБ-23-1                                                  *");
+            Console.WriteLine("*****************************************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                                          *");
+            Console.WriteLine("* Написать программу, которая  вычисляет требуемое значение с использованием тернарного оператора,  *");
+            Console.WriteLine("* где пользователь вводит значение переменной X и Y с клавиатуры.                                   *");
+            Console.WriteLine("*                                                                                                   *");
+            Console.WriteLine("*****************************************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                                                  *");
+            Console.WriteLine("*****************************************************************************************************");
+           
+
+            Console.WriteLine("Введите значение переменной X: ");
+            double x = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите значение переменной Y: ");
+            double y = Convert.ToDouble(Console.ReadLine());
+
+            double res = ds.Calculate(x, y);
+
+            Console.WriteLine("*********************************************************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                                                        *");
+            Console.WriteLine("*********************************************************************************************************************");
+
+            Console.WriteLine("Значение функции =" + res);
+            Console.ReadKey();
+        }
+    }
+}
